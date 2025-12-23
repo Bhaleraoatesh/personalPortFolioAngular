294d17ad361bcdd08feadb26618aa13dfec58f83
Atesh Bhalerao <105929910+Bhaleraoatesh@users.noreply.github.com>
2025-12-23 16:52:17 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  emailJsServiceId: 'emailJsServiceId',
-  emailJsTemplateId: 'emailJsTemplateId',
-  emailJsPublicKey: 'emailJsPublicKey',
-  recaptchaKey:"recaptchaKey"
+  emailJsServiceId: (globalThis as any)['emailJsServiceId'],
+  emailJsTemplateId: (globalThis as any)['emailJsTemplateId'],
+  emailJsPublicKey: (globalThis as any)['emailJsPublicKey'],
+  recaptchaKey:(globalThis as any)['recaptchaKey']
 };
